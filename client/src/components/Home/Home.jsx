61871958e4d9d7cd4fafb4ac8a84455ef8ec28da
SalmannTariq <139531025+SalmannTariq@users.noreply.github.com>
2024-08-18 00:49:38 +0500

    initial commit

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -16,15 +16,15 @@ const Home = () => {
             <section className=' py-5 about-section'>
                 <div className='container'>
                     <div className='row p-3'>
-                        <div className="col-sm-5 d-flex justify-content-center flex-column align-items-center">
+                        <div className="col-md-5 d-flex justify-content-center align-items-center flex-column">
                             <p className='about-text'>ABOUT US</p>
-                            <h3>How We Work</h3>
+                            <h1 className='how-work-text'>How We Work</h1>
                         </div>
-                        <div className="col-sm-7">
-                            <p className='text-center text-sm-start'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs.
+                        <div className="col-md-7">
+                            <p className='text-center text-md-start'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs.
                                 With years of industry experience, our team of experts provides innovative and reliable services that drive success.
                             </p>
-                            <div className='text-center text-sm-start'>
+                            <div className='text-center text-md-start'>
                                 <button className='read-btn'>Read More</button>
                             </div>
                         </div>
@@ -33,7 +33,18 @@ const Home = () => {
 
             </section>
             {/* Services */}
-            <section>
+            <section className='bg-black py-5'>
+                <div className='container'>
+                <div className='d-flex flex-sm-row flex-column gap-3 gap-sm-0 justify-sm-content-between justify-content-center align-items-center'>
+                    <div>
+                    <h5 className='contact-text'>Contact Us</h5>
+                    <h1 className='text-white fw-bold get-text'>GET IN CALL TO <span className='action-text'>ACTION</span></h1>
+                    </div>
+                    <div className='me-sm-5'>
+                        <button className='get-btn'>Get In Touch</button>
+                    </div>
+                </div>
+                </div>
 
             </section>
         </>
