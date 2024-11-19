447c8d2c8b785445db0b7c771b62ab5cec23df16
SalmannTariq <139531025+SalmannTariq@users.noreply.github.com>
2024-11-19 16:52:27 +0500

    initial commit

--- client/src/components/Services/ServiceCards.jsx
+++ client/src/components/Services/ServiceCards.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Services.css';
 // icons
-import webDevelopment from '../assets/webdevelopment.png';
-
-// import { CgWebsite } from "react-icons/cg";
-import { FaBezierCurve } from "react-icons/fa";
-import { FaBookMedical } from "react-icons/fa";
-import { FaCartShopping } from "react-icons/fa6";
-import { FaDesktop } from "react-icons/fa";
-import { HiSpeakerphone } from "react-icons/hi";
+import { FaArrowRight } from "react-icons/fa";
+// images
+import webDevelopment from '../assests/webdevelopment.png';
+import UiUx from '../assests/UiUx.png'
+import MedicalBilling from '../assests/MedicalBilling.png'
+import ECommerce from '../assests/online-shop.png'
+import SoftwareDev from '../assests/software-development.png'
+import MedicalHr from '../assests/Medical-HR.png'
 
 const ServiceCards = () => {
     return (
@@ -16,68 +16,93 @@ const ServiceCards = () => {
             <div className='row my-5'>
                 {/* Web Development */}
                 <div className="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-lg-start">
-                    <div className="card service-card" style={{ width: '22rem', height: "250px" }}>
-                        <div className="position-absolute d-flex flex-column gap-4 align-items-center">
-                            <div className='icon-section'>
-                                <img src={webDevelopment} alt='web-development' className='icon' />
-                                {/* <CgWebsite className='icon' /> */}
+                    <div className="card service-card" style={{ width: '22rem', height: " 300px" }}>
+                        <div className="position-absolute d-flex flex-column gap-2 ">
+                            <div className=' '>
+                                <img src={webDevelopment} alt='web-development' className='icon'  />
                             </div>
+                            <p className='m-0'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs. With years of industry experience, our team of experts provides innovative and reliable services that drive success.</p>
                             <h4>Website Development</h4>
+                            <div className='d-flex align-items-center gap-2'>
+                                <p className='mb-0 more-about-text'>More About</p>
+                                <FaArrowRight className='arrow-icon'/>
+                            </div>
                         </div>
                     </div>
                 </div>
                 {/* UI/UX development */}
                 <div className="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-lg-start mt-5 mt-sm-0">
-                    <div className="card service-card" style={{ width: '22rem', height: "250px" }}>
-                        <div className="position-absolute d-flex flex-column gap-4 align-items-center">
-                            <div className='icon-section'>
-                                <FaBezierCurve className='icon' />
+                    <div className="card service-card" style={{ width: '22rem', height: " 300px" }}>
+                        <div className="position-absolute d-flex flex-column gap-2 ">
+                            <div className=' '>
+                                <img src={UiUx} className='icon' />
                             </div>
+                            <p className='m-0'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs. With years of industry experience, our team of experts provides innovative and reliable services that drive success.</p>
                             <h4>UI / UX</h4>
+                            <div className='d-flex align-items-center gap-2'>
+                                <p className='mb-0 more-about-text'>More About</p>
+                                <FaArrowRight className='arrow-icon'/>
+                            </div>
                         </div>
                     </div>
                 </div>
                 {/* Medical Billing */}
                 <div className="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-lg-start mt-5 mt-lg-0">
-                    <div className="card service-card" style={{ width: '22rem', height: "250px" }}>
-                        <div className="position-absolute d-flex flex-column gap-4 align-items-center">
-                            <div className='icon-section'>
-                                <FaBookMedical className='icon' />
+                    <div className="card service-card" style={{ width: '22rem', height: " 300px" }}>
+                        <div className="position-absolute d-flex flex-column gap-2 ">
+                            <div className=' '>
+                                <img src={MedicalBilling} className='icon' />
                             </div>
+                            <p className='m-0'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs. With years of industry experience, our team of experts provides innovative and reliable services that drive success.</p>
                             <h4>Medical Billing</h4>
+                            <div className='d-flex align-items-center gap-2'>
+                                <p className='mb-0 more-about-text'>More About</p>
+                                <FaArrowRight className='arrow-icon'/>
+                            </div>
                         </div>
                     </div>
                 </div>
                 {/* E-Commerce Development */}
                 <div className="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-lg-start mt-5">
-                    <div className="card service-card" style={{ width: '22rem', height: "250px" }}>
-                        <div className="position-absolute d-flex flex-column gap-4 align-items-center">
-                            <div className='icon-section'>
-                                <FaCartShopping className='icon' />
+                    <div className="card service-card" style={{ width: '22rem', height: " 300px" }}>
+                        <div className="position-absolute d-flex flex-column gap-2 ">
+                            <div className=' '>
+                                <img src={ECommerce} className='icon' />
                             </div>
+                            <p className='m-0'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs. With years of industry experience, our team of experts provides innovative and reliable services that drive success.</p>
                             <h4>E-Commerce Develop</h4>
+                            <div className='d-flex align-items-center gap-2'>
+                                <p className='mb-0 more-about-text'>More About</p>
+                                <FaArrowRight className='arrow-icon'/>
+                            </div>
                         </div>
                     </div>
                 </div>
                 {/* Software development */}
                 <div className="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-lg-start mt-5">
-                    <div className="card service-card" style={{ width: '22rem', height: "250px" }}>
-                        <div className="position-absolute d-flex flex-column gap-4 align-items-center">
-                            <div className='icon-section'>
-                                <FaDesktop className='icon' />
+                    <div className="card service-card" style={{ width: '22rem', height: " 300px" }}>
+                        <div className="position-absolute d-flex flex-column gap-2 ">
+                            <div className=' '>
+                                <img src={SoftwareDev} className='icon' />
                             </div>
+                            <p className='m-0'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs. With years of industry experience, our team of experts provides innovative and reliable services that drive success.</p>
                             <h4>Software Development</h4>
                         </div>
                     </div>
                 </div>
                 {/* Medical HR Management */}
                 <div className="col-lg-4 col-sm-6 col-12 d-flex justify-content-center justify-content-lg-start mt-5">
-                    <div className="card service-card" style={{ width: '22rem', height: "250px" }}>
-                        <div className="position-absolute d-flex flex-column gap-4 align-items-center">
-                            <div className='icon-section'>
-                                <HiSpeakerphone className='icon' />
+                    <div className="card service-card" style={{ width: '22rem', height: " 300px" }}>
+                        <div className="position-absolute d-flex flex-column gap-2">
+                            <div className=' '>
+                                <img src={MedicalHr} className='icon' />
                             </div>
+                            <p className='m-0'>At PeerLogics, we are dedicated to delivering exceptional technology solutions tailored to your business needs. With years of industry experience, our team of experts provides innovative and reliable services that drive success.</p>
                             <h4>Medical HR Management</h4>
+                            <div className='d-flex align-items-center gap-2'>
+                                <p className='mb-0 more-about-text'>More About</p>
+                                <FaArrowRight className='arrow-icon'/>
+                            </div>
                         </div>
                     </div>
                 </div>
